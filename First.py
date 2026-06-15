0c5cefc0b8c9b4235636a20ebeadeb74cbad1980
g1br1l <techgib.hourcard@icloud.com>
2026-06-15 15:44:46 +0200

    three axes

--- First.py
+++ First.py
@@ -1,20 +1,56 @@
 import pygame
 import sys
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 import numpy as np
 
-pygame.init()
+def draw_axes():
+    glBegin(GL_LINES)
+    glColor3f(1, 0, 0) # red 
+    glVertex3f(0, 0, 0)
+    glVertex3f(10, 0, 0)
 
-screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption("missile simulation")
+    glColor3f(0, 1, 0) # green 
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 10, 0)
 
-clock = pygame.time.Clock()
+    glColor3f(0, 0, 1) # blue
+    glVertex3f(0, 0, 0)
+    glVertex3f(0, 0, 10)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    screen.fill((255,255,255))
-    pygame.display.flip()
-    clock.tick(60)
+    glEnd()
+
+def init_gl(width, height):
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, width / height, 0.1, 200.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    gluLookAt(20, 20, 20,
+              0, 0, 0,
+              0, 1, 0)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800,600), DOUBLEBUF | OPENGL)
+    pygame.display.set_caption("missile simulation")
+    clock = pygame.time.Clock()
+
+    init_gl(800, 600)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glClearColor(0.1, 0.1, 0.15, 1)
+
+        draw_axes()
+        pygame.display.flip()
+        clock.tick(60)
+
+main()
